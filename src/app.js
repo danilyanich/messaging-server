2a7b1357dfd744ae81ff7b1d25c1dbac300d8b9a
danilyanich <danilyanich@gmail.com>
2018-07-25 20:02:57 +0300

    [+] Add heroku configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const logger = require('koa-logger');
 
+const routes = require('./routes');
+
 const app = new Koa();
 
 app.use(bodyparser());
@@ -9,6 +11,7 @@ app.use(logger());
 
 routes(app);
 
-app.listen(3000, () => {
-  console.log('App is listening on http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is listening on http://localhost:${port}`);
 });
